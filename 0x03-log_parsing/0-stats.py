827c84d99601e4be52515019ffd396b5ac01843e
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-16 02:22:47 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,24 +14,20 @@ counter = 0
 
 try:
     for line in sys.stdin:
-        counter += 1
         data = line.split()
-        try:
+        if len(data) > 4
             file_size += int(data[-1])
-        except:
-            pass
-        try:
             if data[-2] in status_codes:
                 status_codes[data[-2]] += 1
-        except:
-            pass
+        counter += 1
+
         if counter == 10:
             print("File size: {}".format(file_size))
             for key in sorted(status_codes.keys()):
                 if status_codes[key] != 0:
                     print("{}: {}".format(key, status_codes[key]))
             counter = 0
-except KeyboardInterrupt:
+except Exception as err:
     pass
 finally:
     print("File size: {}".format(file_size))
