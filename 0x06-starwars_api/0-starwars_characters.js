5eb1dc11e0da992001469f3c6261fb5f05b1779a
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-14 02:35:46 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,24 +4,25 @@
  */
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 
-request(url, async (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      await new Promise((resolve, reject) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            console.log(JSON.parse(body).name);
-            resolve();
-          }
-        });
+const filmNum = process.argv[2] + '/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + filmNum, async (err, res, data) => {
+  if (err) return console.error('Error:', err);
+
+  // find URLs of each character in the film as a list obj
+  const characters = JSON.parse(data).characters;
+
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, data) => {
+        if (err) return console.error('Error:', err);
+
+        // print each character name and prints in URL order
+        console.log(JSON.parse(data).name);
+        resolve();
       });
-    }
+    });
   }
 });
